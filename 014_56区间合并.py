5bd2ec38a0801ba3dde99e2a668f87dbc7be2bb6
“zhuzhuyumeng” <877247564@qq.com>
2025-09-16 19:58:16 +0800

    014

--- 014_56区间合并.py
+++ 014_56区间合并.py
@@ -1,8 +1,11 @@
 class Solution:
     def merge(self, intervals: list[list[int]]) -> list[list[int]]:
-
+        intervals.sort(key=lambda p:p[0])
+        ans = []
+        for p in intervals:
+            print(p[0])
 
 
 
 intervals = [[1,3],[2,6],[8,10],[15,18]]
-print(Solution.maxSubArray(Solution,intervals))
+print(Solution.merge(Solution,intervals))
