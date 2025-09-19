39518e296a626934f7e4bef324cde5fb81f48989
“zhuzhuyumeng” <877247564@qq.com>
2025-09-19 20:46:50 +0800

    25常规复制回文

--- hot100/025_234回文链表.py
+++ hot100/025_234回文链表.py
@@ -5,3 +5,18 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        cur = head
+        ans = []
+        while cur:
+            ans.append(cur.val)
+            cur = cur.next
+        n = len(ans)
+        left = 0
+        right = n-1
+        flag = True
+        while left<right:
+            if ans[left]!=ans[right]:
+                flag=False
+            left+=1
+            right-=1
+        return flag
