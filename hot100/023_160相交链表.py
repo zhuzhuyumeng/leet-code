3859e61c24358e02b1d4eaff00b049c92ab430e7
“zhuzhuyumeng” <877247564@qq.com>
2025-09-19 19:50:18 +0800

    23

---
+++ hot100/023_160相交链表.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        p = headA
+        q = headB
+        while p is not q:
+            if p:
+                p = p.next
+            else:
+                p = headB
+            if q:
+                q = q.next
+            else:
+                q = headA
+        return p
